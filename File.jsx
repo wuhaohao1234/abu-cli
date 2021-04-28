31b26961eae7482bd5cb0acd55054085d4dd8ebd
wuhaohao1234 <1611499758@qq.com>
2021-04-28 23:23:17 +0800

    feat: init

---
+++ File.jsx
@@ -0,0 +1,20 @@
+
+import * as React from 'react';
+
+export class File extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {}
+    }
+
+    componentDidMount(){
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+const version = require('../package.json').version
+const fs = require('fs')
+const reactTemplate = require('./template/reactClass')
+const term = require('terminal-kit').terminal;
+
+const logo = `
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌           ▀▀▀▀█░█▀▀▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     
+▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     
+▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄█░█▄▄▄▄ 
+▐░▌       ▐░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▀         ▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
+`
+
+// term.bgBlack(logo+ '\n')
+program
+    .version(version,'-v, --version')
+    .command('init')
+    .description('initialize your meet config')
+    .action(() => {
+      console.log('init')
+    });
+program
+    .command('create <filename>')
+    .description('创建一个文件')
+    .action( async (filename) => {
+        const str = reactTemplate(filename)
+        fs.writeFileSync(filename + '.jsx', str)
+    })
+program.parse(process.argv);
